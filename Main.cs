f39c4a7887771915590dd5df5bf937f4a8209957
gompocp <73906388+gompocp@users.noreply.github.com>
2021-02-02 20:32:48 +0000

    Bump Ver. and Update for Build 1046

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UIExpansionKit.API;
 using MelonLoader;
 using UnhollowerRuntimeLib;
@@ -6,42 +7,47 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-
 namespace DownloadFix
 {
     public static class ModInfo
     {
         public const string Name = "DownloadFix";
         public const string Author = "gompo#6956";
-        public const string Version = "1.0.1";
-        public const string DownloadLink = null;
+        public const string Version = "1.0.2";
+        public const string DownloadLink = "https://github.com/gompocp/DownloadFix/releases";
     }
 
     public class DownloadFix : MelonMod
     {
+        public static Queue<string> downloadQueue = new Queue<string>();
+
         public override void OnApplicationStart()
         {
             ICustomLayoutedMenu settingsPage = ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu);
-            settingsPage.AddSimpleButton("Unblock unpack queue", delegate
-            {
-                Utilities.UnblockUnPackQueue();
-            });
+            settingsPage.AddSimpleButton("Unblock unpack queue", delegate { Utilities.UnblockUnPackQueue(); });
         }
 
         public override void VRChat_OnUiManagerInit()
         {
-            GameObject unblockButton = GameObject.Instantiate(GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel/Parent_Loading_Progress/GoButton").transform, GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel").transform).gameObject;
+            GameObject unblockButton = GameObject
+                .Instantiate(
+                    GameObject.Find(
+                            "UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel/Parent_Loading_Progress/GoButton")
+                        .transform,
+                    GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel").transform)
+                .gameObject;
             unblockButton.GetComponentInChildren<Text>().text = "Unblock Queue";
             unblockButton.GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
-            unblockButton.GetComponent<Button>().onClick.AddListener(DelegateSupport.ConvertDelegate<UnityAction>(new Action(delegate
-            {
-                Utilities.UnblockUnPackQueue();
-                Utilities.DeselectClickedButton(unblockButton);
-            })));
+            unblockButton.GetComponent<Button>().onClick.AddListener(DelegateSupport.ConvertDelegate<UnityAction>(
+                new Action(delegate
+                {
+                    Utilities.UnblockUnPackQueue();
+                    Utilities.DeselectClickedButton(unblockButton);
+                })));
             unblockButton.GetComponent<Transform>().localPosition = new Vector3(-2.4f, -124f, 0);
             GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel/Parent_Loading_Progress")
                 .transform.localPosition = new Vector3(0, 17, 0);
-            
+
         }
     }
 }
--- Utilities.cs
+++ Utilities.cs
@@ -8,7 +8,6 @@ namespace DownloadFix
         public static void UnblockUnPackQueue()
         {
             AssetBundleDownloadManager.prop_AssetBundleDownloadManager_0.field_Private_Boolean_0 = false; //Yes this literally fixes it
-            //Not 100% sure if this causes some running coroutines to run infinitely in the background... I wouldn't think so
         }
         
         public static void DeselectClickedButton(GameObject button)
